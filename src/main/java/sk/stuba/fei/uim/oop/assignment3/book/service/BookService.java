1a2a504e14a2122b92ad7887fec8e9d1d229de4a
parchemaster <86682020+parchemaster@users.noreply.github.com>
2022-05-02 17:54:04 +0200

    passed 22 tests

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/book/service/BookService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/book/service/BookService.java
@@ -11,6 +11,7 @@ import sk.stuba.fei.uim.oop.assignment3.exception.NotFoundException;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService implements IBookService{
@@ -35,39 +36,42 @@ public class BookService implements IBookService{
 //    @SneakyThrows
     @Override
     public Book create(BookAddRequestBody request) throws NotFoundException {
-//        var authorsID = authorService.getAll().stream().map(Author::getId).collect(Collectors.toList());
-//        if (authorsID.contains(request.getAuthorId())) {
-//            var author = authorService.getById(request.getAuthorId());
-//            var newBook = new Book(request, author);
-//            if (authorsID.contains(request.getAuthorId())) authorService.getById(request.getAuthorId()).getBooks().add(newBook);
-//            return this.repository.save(newBook);
-//        }
-
-//        var author = authorService.getById(request.getAuthorId());
-        if (request.getAuthor() != null) {
-            if (authorService.getById(request.getAuthor()) != null
-                    && authorService.getById(request.getAuthor()).getId().equals(request.getAuthor())) {
-                var newBook = new Book(request, authorService.getById(request.getAuthor()));
-                authorService.getById(request.getAuthor()).getBooks().add(newBook);
-                return this.repository.save(newBook);
-            }
-        }
-        return null;
+        var newBook = new Book(request, authorService.getById(request.getAuthor()));
+        authorService.getById(request.getAuthor()).getBooks().add(newBook);
+        return this.repository.save(newBook);
     }
 
     @Override
     public Book update(long id, BookUpdateRequestBody request) throws NotFoundException {
         var updatedBook = this.getById(id);
-        updatedBook.setName(request.getName());
-        updatedBook.setDescription(request.getDescription());
-        updatedBook.setAuthor(authorService.getById(request.getAuthor()));
-        updatedBook.setPages(request.getPages());
+        var isReplacedAuthor = false;
+        if (!request.getAuthor().equals(updatedBook.getAuthor().getId()) && request.getAuthor() != null) {
+            authorService.getById(updatedBook.getAuthor().getId()).getBooks().remove(updatedBook);
+            isReplacedAuthor = true;
+        }
+        updatedBook.setName(request.getName() != null ? request.getName() : updatedBook.getName());
+        updatedBook.setDescription(request.getDescription() != null ? request.getDescription() : updatedBook.getDescription());
+        updatedBook.setAuthor(request.getAuthor() != null && request.getAuthor() != 0 ? authorService.getById(request.getAuthor()) : updatedBook.getAuthor());
+        updatedBook.setPages(request.getPages() != null && request.getPages() != 0 ? request.getPages() : updatedBook.getPages());
+
+//        if (isReplacedAuthor) {
+//            authorService.getById(updatedBook.getAuthor().getId()).getBooks().add(updatedBook);
+//        }
+
         return this.repository.save(updatedBook);
     }
 
     @Override
     public void delete(long id) throws NotFoundException {
-        this.repository.delete(this.getById(id));
+        var deletedBook = this.getById(id);
+        var authors = authorService.getAll().stream().filter(author -> author.getBooks().contains(deletedBook)).collect(Collectors.toList());
+//        if (authorService.getAll().stream().anyMatch(author -> author.getBooks().contains(deletedBook))) {
+//
+//        }
+        for (var author : authors) {
+            author.getBooks().remove(deletedBook);
+        }
+        this.repository.delete(deletedBook);
     }
 
     @Override
